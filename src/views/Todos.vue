c1f213669a2a7f56d8347affd96a33339aaf93c8
HaruoNakachi <nakachi.haruo@biz-app.biz>
2019-12-23 04:20:38 +0900

    console.log を eslint で許可する

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -2,53 +2,62 @@
   <div id="app">
     <button @click="createNewTodo">Add Todo</button>
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        {{todo.name}} - {{todo.description}} - {{todo.id}}
-      </li>
+      <li v-for="todo in todos" :key="todo.id">{{todo.name}} - {{todo.description}} - {{todo.id}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import API, {  graphqlOperation } from '@aws-amplify/api';
+import API, { graphqlOperation } from "@aws-amplify/api";
 // other imports
-import { createTodo } from '../graphql/mutations'
-import { listTodos } from '../graphql/queries'
-import { onCreateTodo } from '../graphql/subscriptions'
+import { createTodo } from "../graphql/mutations";
+import { listTodos } from "../graphql/queries";
+import { onCreateTodo } from "../graphql/subscriptions";
 
 export default {
-  name: 'app',
-  data(){
+  name: "app",
+  data() {
     return {
       todos: []
-    }
+    };
   },
-  methods :{
-    async createNewTodo(){
-      const todo = { name: "Use AppSync" , description: "Realtime and Offline"}
-      await API.graphql(graphqlOperation(createTodo, { input: todo }))
+  methods: {
+    async createNewTodo() {
+      const todo = { name: "Use AppSync", description: "Realtime and Offline" };
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
     },
-    async getData(){
-      const todoData = await API.graphql(graphqlOperation(listTodos))
+    async getData() {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
       this.todos.push(...this.todos, ...todoData.data.listTodos.items);
     },
-    subscribe(){
-      API.graphql(graphqlOperation(onCreateTodo))
-      .subscribe({
-        next: (eventData) => {
-          const todo = eventData.value.data.onCreateTodo;
-          this.todos.push(todo);
-        },
-        error: (err) => {
-          alert("Subscription Error: " + err.message)
-        }
-      })
+    subscribe() {
+      setTimeout(() => {
+        const subscription =
+          API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+            next: eventData => {
+              const todo = eventData.value.data.onCreateTodo;
+              this.todos.push(todo);
+            },
+            error: err => {
+              alert("Subscription Error: " + err.message);
+              const result = confirm("再接続しますか？");
+              if (result) {
+                setTimeout(() => {
+                  subscription.unsubscribe();
+                  alert("UNSUBSCRIBED")
+                  this.subscribe();
+                }, 1000);
+              } else {
+                alert("再接続しませんでした。")
+              }
+            }
+          });
+      }, 1000)
     }
   },
-  created(){
-    this.getData()
-    this.subscribe()
+  created() {
+    this.getData();
+    this.subscribe();
   }
 };
-
 </script>
