960de89fdf548ed45ac805ed955e453678c67a8c
HaruoNakachi <nakachi.haruo@biz-app.biz>
2019-12-24 23:37:27 +0900

    pdfmake で日本語を出力できるように修正

--- src/plugins/pdfmakeDefinitionBuilder.js
+++ src/plugins/pdfmakeDefinitionBuilder.js
@@ -3,9 +3,12 @@ const builder = {
     console.log('TEST')
     const docDefinition = {
       content: [
-        'First paragraph',
+        '日本語のテスト',
         'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
-      ]
+      ],
+      defaultStyle: {
+        font: 'GenShin',
+      },
     };
     return docDefinition
   }
--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -21,6 +21,14 @@ import pdfFonts from "pdfmake/build/vfs_fonts";
 import pdfDefinitionBuilder from '../plugins/pdfmakeDefinitionBuilder';
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
+pdfMake.fonts = {
+  GenShin: {
+    normal: 'GenShinGothic-Normal-Sub.ttf',
+    bold: 'GenShinGothic-Normal-Sub.ttf',
+    italics: 'GenShinGothic-Normal-Sub.ttf',
+    bolditalics: 'GenShinGothic-Normal-Sub.ttf',
+  },
+};
 
 export default {
   name: 'customers',
